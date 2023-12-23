f5e1e758e753a12cfda2e9b8f5b28c8ebaf56ee6
jfgl <2816804692101@ingenieria.usac.edu.gt>
2023-12-23 16:02:16 -0600

    arregle de la calculadora

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // esto agrega a la pantala el vaor de la calculadora que agreguemos 
 
 function agregar(valor){
-    document.getElementById('pantalla').value += valor
+    document.getElementById('pantalla').value += valor;
 }
 
 function calcular(){
-    const respantalla= document.getElementById('pantalla').value
+    const respantalla= document.getElementById('pantalla').value;
     const resultado= eval(respantalla)
-    document.getElementById('pantalla').value = resultado
+    document.getElementById('pantalla').value = resultado;
 
 }
 
 function borrar(){
-    document.getElementById('pantalla').value = ''
+    document.getElementById('pantalla').value = '';
+}
+
+function deshacer(){
+    const respantalla= document.getElementById('pantalla').value;
+    document.getElementById('pantalla').value = respantalla.slice(0,-1)
 }
